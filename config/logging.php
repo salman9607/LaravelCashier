b3d4f6c528a075a60dd3f0d11b96399d49443dcb
root <root@server.kpfraser.com>
2022-10-21 16:22:31 +0000

    embed flare

--- config/logging.php
+++ config/logging.php
@@ -53,7 +53,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single', 'flare'],
             'ignore_exceptions' => false,
         ],
 
@@ -116,6 +116,9 @@ return [
 
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
+        ],
+        'flare' => [
+            'driver' => 'flare',
         ],
     ],
 
